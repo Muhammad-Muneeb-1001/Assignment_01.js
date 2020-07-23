69c1fe47f28501ff5967c24fa13580131f5bcd12
Muhammad Muneeb <hornetarmour1001@gmail.com>
2020-07-24 02:35:47 +0500

    added index.html
    Saylani-JavaScriptAssignment
    [Chapter# 1-67]

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+
+
+// --------------------ASSIGNMENT: CHAPTER# 1-20------------------------------------
+// --------------------ASSIGNMENT: CHAPTER# 1-20------------------------------------
+// --------------------ASSIGNMENT: CHAPTER# 1-20------------------------------------
+
+
+
+
 //chapter 01
 
 // alert("Welcome to our Website")
@@ -51,7 +60,7 @@
 
 // Chapter# 04
 
-//var one = 1, two = 'two', three;
+// var one = 1, two = 'two', three;
 
 // // Legal Variables
 // var legalVariable = 1;
@@ -79,7 +88,7 @@
 //     '.', "For example $name, _name or name"
 //     < br > "Variable names are case" + e + "Variable names should not be JS " + f + )
 
-                                //Chapter# 5
+//                                 Chapter# 5
 
 // var a = 5;
 // var b = 3;
@@ -825,6 +834,523 @@
 //     document.write("<br><br>Element at index 3 is: " +fruits[3]);
 //     document.write("<br><br>Element at index 4 is: " +fruits[4]);
 // }
+
+
+
+
+
+
+
+// --------------------ASSIGNMENT: CHAPTER# 21-40-----------------
+// --------------------ASSIGNMENT: CHAPTER# 21-40-----------------
+// --------------------ASSIGNMENT: CHAPTER# 21-40-----------------
+
+
+
+
+
+
+
+// Chapter# 21-25:
+
+
+
+// var input1 = prompt("Enter first name","input1");
+// var input2 = prompt("Enter last name"," input2");
+// var fullName = input1 + input2;
+// alert("Welcome " + fullName);
+
+// var user = prompt("Your favorite mobile phone", "Enter");
+// document.write("My favorite phone is: " + user)
+// var stringLength = str.length[user];
+// document.write("Length of string is " + stringLength);
+
+// var word = "Pakistani";
+// document.write("String: " + word);
+// document.write(" <br> Index of 'n' is: " + word.indexOf("n"));
+
+
+// var word = ["Hello World"];
+// document.write("String: " + word);
+// document.write(" <br> Last index of 'l' is: " + word.lastIndexOf("l"));
+
+
+// var text = "Pakistani";
+// var firstChar = text.charAt(3)
+// document.write("String is: " + text);
+// document.write("<br>  Character at index 3 is: " + firstChar);
+
+
+// var input1 = prompt("Enter first name","input1");
+// var input2 = prompt("Enter last name"," input2");
+// var fullName = input1.concat ( input2);
+// alert("Welcome " + fullName);
+
+// var city1 = "Hyderabad";
+// var city2 = city1.replace("Hyder", "Islam");
+// document.write("City: " + city1);
+// document.write(" <br> After Replacement: "+ city2);
+
+// var message = ("Ali and Sami are best friends, They play cricket and football together");
+// var newMessage = message.replace(/and/g, "&");
+// document.write(newMessage);
+
+
+
+// var string1 = Number("472") ;
+// var number1 = 472;
+// document.write("Value: " + string1);
+// document.write(" <br>Type: String");
+// document.write(" <br>Value: " + number1.toString());
+// document.write(" <br>Type: Number");
+
+
+// var user1 = prompt("Enter", "word");
+// var convertedVar = user1.toUpperCase();
+// document.write(convertedVar);
+
+
+
+// function titleCase(string) {
+//     var user = prompt("Enter", "word");
+
+//     var user1 = string.toLowerCase().split(" ");
+//     for(var i = 0; i< user1.length; i++){
+//         user1[i] = user1[i][0].toUpperCase() + user1[i].slice(1);
+//     }
+//  document.write(user1.join(" "));
+//  return user1;
+//  }
+//  titleCase(user1);
+
+// var user1 = prompt("Enter", "word");
+// var convertedVar = user1.titleCase();
+// document.write(convertedVar);
+
+
+// var number1 = 35.36;
+// var newNumber= message.replace("35.36", "3536");
+// document.write(newNumber);
+
+
+
+// var username = "cm@d"
+// for(var i = 0; i < username.length; i++){
+//     if( username [i].charCodeAt() === 33 || 44 || 46 || 64 ){
+//         alert("Username is Invalid!");
+//     }
+    
+//         else if( username [i].charCodeAt() === 44){
+//             alert("Username is Invalid!");
+//     }
+    
+//        else if( username [i].charCodeAt() === 46){
+//                 alert("Username is Invalid!");
+//             }    
+
+//         else if( username [i].charCodeAt() === 64){
+//                     alert("Username is Invalid!");
+//     }
+//      else{
+//          alert("Username is Valid!")
+//  }        
+// }
+
+
+
+// var userItem = prompt("Enter", "item");
+// var  Bakery = userItem.indexOf(["cake", "apple pie", "cookie", "chips", "patties"]);
+// var notAvail = ["pastry"]
+// if ( userItem = Bakery.search() === /cookIes,cOokies,COOKIES/i)
+// {
+// alert("cookie i availanle at index: " + Bakery + "in our bakery");
+// }
+// else{
+// alert("We are sorry. " + notAvail + "in our bakery" );
+// }
+
+
+
+// var userPassword = prompt("Enter", "password")
+// for(var i = 0; i < userPassword.length; i++){
+    
+//     if( userPassword [i].charCodeAt() === 48 || 49||50||51||52||53||54||55||56||57||65||66||67||68||69||70||71||72||73||74||75||76||77||78||79||80||81||82||83||84||85||86||87||88||89||90||97||98||99||100||101||102||103||104||105||106||107||108||109||110||111||112||113||114||115||116||117||118||119||120||121||122){
+//         alert("User Password is Valid!");
+//     }
+//     else if (isNaN(parseInt(userPassword[0]))) {
+//         alert("password is valid");
+//       } 
+//     else if {}
+//         alert("Password should not start with number")
+//       }
+
+//     else{
+//        alert("Write a valid password") 
+//     }
+
+
+// var university = "University of Karachi";
+// var val = new Array();
+// val =university.split("");
+// for (i=0;i<val.length;i++)
+// {
+// document.write(val[i] + "<br >");
+// }
+
+// var user = prompt("Enter");
+// var word = user.inderOf("Pakistan");
+// document.write("String: " + word);
+// document.write(" <br> Index of 'n' is: " + word.indexOf("n"));
+
+// var string1 = ("The quick brown fox jumps over the lazy dog");
+// var res = string1.search(the);
+// document.write("There are " + res + "occurences of the in the string");
+
+
+
+
+
+// function search_word(text, word){
+    
+//     var x = 0, y=0;
+   
+//     for (i=0;i< text.length;i++)
+//         {
+//         if(text[i] == word[0])
+//             {
+//             for(j=i;j< i+word.length;j++)
+//                {
+//                 if(text[j]==word[j-i])
+//                   {
+//                     y++;
+//                   }
+//                 if (y==word.length){
+//                     x++;
+//                 }
+//             }
+//             y=0;
+//         }
+//     }
+//    return "'"+word+"' was found "+x+" times.";
+// }
+// document.write(search_word('the quick brown fox jumps over the lazy dog', 'the'));
+// document.write("<br>"+search_word('the'));
+
+
+
+// Chapter# 26-30:
+
+
+// var positiveNum = prompt("Enter","num");
+// if (positiveNum >= 0){
+//     document.write("Number is negative"+Math.ceil(positiveNum))
+//   document.write("Number is negative"+Math.round(positiveNum))
+//   document.write("Number is negative"+Math.floor(positiveNum))
+//  document.write("Number is negative"+(positiveNum)) 
+// }
+// else {
+
+// }
+
+// var negativeNum = prompt("Enter","num");
+// if (negativeNum <= 0){
+//     document.write("Number is negative"+Math.ceil(negativeNum))
+//   document.write("Number is negative"+Math.round(negativeNum))
+//   document.write("Number is negative"+Math.floor(negativeNum))
+//  document.write("Number is negative"+(negativeNum)) 
+//}
+// else {
+// }
+
+
+// var absValue1 = Math.abs(-4);
+// var absValue2 = Math.abs(-5);
+// document.write(absValue1);
+// document.write("<br>" + absValue2);
+
+
+// var diceValue1 = Math.floor(Math.random() * 7);
+// var diceValue2 = Math.floor(Math.random() * 7);
+// document.write( "random dice value: " + diceValue1);
+// document.write("<br> random dice value" + diceValue2);
+
+
+// var coinValue1 = Math.floor(Math.random() * 2);
+// var coinValue2 = Math.floor(Math.random() * 2);
+// document.write( "random coin value: " + coinValue1);
+// document.write("<br> random coin value" + coinValue2);
+
+
+// var randNum = Math.floor(Math.random() * 101);
+// document.write("random number between 1 and 100 is: " + randNum);
+
+
+// var userWeight = prompt("Enter", "weight in Kg");
+// var num = userWeight.toString();
+// document.write("The weight of user is: " + num + " Kg.");
+
+
+// var userNum = prompt("Enter" , "number");
+// var secretNum = Math.floor(Math.random() * 11);
+// if (secretNum == userNum){
+// alert("Congratulations")
+// }
+// else{
+//     alert("Try again")
+// }
+
+
+
+// Chapter# 31-34:
+
+
+// var a = new Date();
+// document.write(a);
+
+// var b = a.getMonth();
+// document.write( "<br>"+b);
+
+// var c= a.toString()
+// var d = c.slice(0,3); 
+// document.write("<br>" +d)
+// //alert( "<br>" + d);
+
+// var day = c.getDay();
+// if(day == Sat || Sun){
+//    document.write("It is a Fun Day") 
+// }
+// else{
+// }
+
+// var a = new Date();
+// if(a <= 15 ){
+//     document.write("First fifteen days of the month" + a);
+// }
+// else(a > 15 || a == 30){
+//     document.write("last days of the month" + a);
+// }
+
+// var date = new Date(prompt("Enter", "date"));
+// var dateMili = date.getTime();
+// var today = new Date();
+// var todayMili =  today.getTime();
+// var diff =  todayMili - dateMili ;
+// var accuMin = Math.floor(diff/(1000*60*60)) 
+ 
+// document.write(accuMin);
+// // document.write("Current Time: " + a);
+
+
+// var date = new Date("Thu December 31, 2020 00:00:00 GMT+0500 (PKT) ");
+// lastDate = date;
+// document.write(lastDate);
+
+
+// var customerName = "ABC";
+// var month = "February";
+// var numberOfUnits = "410";    
+// var chargesPerUnit = 16;
+// var netAmount = 6560;
+// var latePayment = 350;
+// var grossAmount = 6910;
+// document.write(<h1>K-Electric Bill</h1>)
+// document.write(" <br>Customer Name: " + customerName);
+// document.write(" <br>Billing Month: " + month);
+// document.write(" <br>Number of Units: " + numberOfUnits);
+// document.write(" <br>Charges per unit: " + chargesPerUnit);
+// document.write("<br>");
+// document.write("<br>");
+// document.write("Net Amount Payble (within Due Date):" + netAmount);
+// document.write("Late payment surcharge: " + latePayment);
+// document.write("Gross Amount Payble (after Due Date) " + grossAmount);
+
+
+
+// Chapter# 43-48:
+
+// function foo(){
+//     alert("Hello, World!")
+// }
+// document.write()
+
+// <!-- <a href = "#" onclick = "foo()">Click Me</a>
+// -->
+// <!-- <img id="example" onclick="alert('Thanks for purchasing a phone from us.')" width= "100" src="img1.jpg" alt="">
+// <img onclick="alert('Thanks for purchasing a phone from us.')" width= "100" src="img2.jpg" alt="">
+// <img onclick="alert('Thanks for purchasing a phone from us.')" width= "100" src="img3.webp" alt=""> -->
+
+
+// <!-- <img width="500" height="750" src='img1.jpg' onmouseover="this.src='img2.jpg';" onmouseout="this.src='img1.jpg';" />
+
+// -->
+
+
+
+//                              Chapter# 52-57
+
+
+// function showImage(e){
+//   var modalImage = document.getElementById("modalImage");
+  
+//   modalImage.src = e.src
+ 
+// }
+
+{/* <div>
+
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal" class = "img" src="1.jpg" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal" class = "img" src="2.jpg" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal" class = "img" src="3.jpg" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal" class = "img" src="4.png" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal" class = "img" src="5.jpg" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal" class = "img" src="6.jpg" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal" class = "img" src="7.png" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal"  class = "img" src="8.jpg" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal"  class = "img" src="9.jpg" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal"  class = "img" src="10.jpg" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal"  class = "img" src="11.jpg" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal"  class = "img" src="12.jpg" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal"  class = "img" src="13.jpg" alt="">
+<img onclick="showImage(this)" data-toggle="modal" data-target="#exampleModal"  class = "img" src="14.png" alt="">
+
+ 
+</div>
+<!-- Button trigger modal
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+Launch demo modal
+</button> -->
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog">
+<div class="modal-content">
+<div class="modal-header">
+<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+<span aria-hidden="true">&times;</span>
+</button>
+</div>
+<div class="modal-body">
+
+<img class="bigImage" src="" alt="" id="modalImage">
+
+
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+<button type="button" class="btn btn-primary">Save changes</button>
+</div>
+</div>
+</div>
+</div>
+
+ */}
+
+
+
+
+
+
+//                                  Chapter# 58-67:
+
+
+
+
+// var m = document.getElementsByTagName('div');
+// console.log(m)
+
+// var b = document.getElementsByClassName('main-content');
+// console.log(b.childNodes)
+
+// var c = document.getElementByClass('render');
+// var contents= p[4].innerHTML; 
+
+// var d = document.getElementById('first-name');
+// var text = document.createTextNode('Mr. ABC');
+// d.appendChild(text)
+
+// var e = document.getElementById('first-name');
+// var text = document.createTextNode('Mr. XYZ');
+// e.appendChild(text)
+// var f = document.getElementById('email');
+// var text = document.createTextNode('example@xyz.com');
+// f.appendChild(text)
+
+ 
+
+ 
+ 
+
+
+
+
+
+// var a = document.getElementById('form-content')
+// console.log(a.nodeType)
+
+// var b = document.getElementById('lastName')
+// console.log(b.childNodes.nodeType)
+
+// var c = document.getElementById('main-content')
+// console.log(c.firstChild)
+// console.log(c.lastChild)
+
+// var firstE1 = document.getElementById('lastName');
+// console.log(firstE1.nextSibling)
+// var secondE2 = document.getElementById('lastName');
+// console.log(secondE2.previousSibling)
+
+// var e = document.getElementById('email');
+// console.log(e.parentNode)
+// var e = document.getElementById('email');
+// console.log(e.nodeType)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
